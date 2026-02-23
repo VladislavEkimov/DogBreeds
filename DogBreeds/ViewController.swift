da8bb4e8bee0c85a1541082849b358ff3d897d6b
vladislav <vladis27449@yandex.ru>
2026-02-23 11:17:05 +0700

    Initial Commit

---
+++ DogBreeds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogBreeds
+//
+//  Created by Владислав on 23.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
